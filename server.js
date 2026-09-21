2f8aee6f86aff8cdbf9703748a3014b3844fe12a
lebrona777-del <lebrona777@gmail.com>
2026-09-21 22:22:26 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,52 @@
+{
+  id: "spaeri-dila-2026-09-21",
+  sport: "football",
+  championship: "ეროვნული ლიგა",
+
+  homeTeam: {
+    name: "სპაერი",
+    shortName: "სპა"
+  },
+
+  awayTeam: {
+    name: "დილა",
+    shortName: "დილ"
+  },
+
+  date: "2026-09-21",
+  time: "21:00",
+
+  status: "live",
+
+  minute: 70,
+
+  score: {
+    home: 1,
+    away: 4
+  },
+
+  events: [],
+
+  statistics: {
+    possession: {
+      home: 46,
+      away: 54
+    },
+    shots: {
+      home: 7,
+      away: 12
+    },
+    shotsOnTarget: {
+      home: 3,
+      away: 7
+    },
+    corners: {
+      home: 4,
+      away: 6
+    },
+    fouls: {
+      home: 10,
+      away: 9
+    }
+  }
+}
